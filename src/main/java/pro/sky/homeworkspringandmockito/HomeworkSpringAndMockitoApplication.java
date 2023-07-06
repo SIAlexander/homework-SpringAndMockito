8cfcbc7a91a59e94c25ddfe5e9a893ec5f921a76
Smokvin <alex.smokvin@gmail.com>
2023-07-06 13:02:08 +0300

    initial commit

---
+++ src/main/java/pro/sky/homeworkspringandmockito/HomeworkSpringAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.homeworkspringandmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringAndMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkSpringAndMockitoApplication.class, args);
+    }
+
+}
